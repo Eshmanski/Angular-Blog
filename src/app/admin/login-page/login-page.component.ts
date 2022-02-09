42f2691a18381b6b826ae58a0b29dc9ad31811f1
Pavel Eshmanski <Eshmanski@gmail.com>
2022-02-09 18:14:50 +0300

    12: Create Guard for routing

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../../shared/interface";
 import {AuthService} from "../shared/services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -12,13 +12,21 @@ import {Router} from "@angular/router";
 export class LoginPageComponent implements OnInit {
   form: FormGroup = null!;
   submitted: boolean = false;
+  message: string = '';
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute,
     ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if(params['loginAgain']) {
+        this.message = 'Пожалуйста введите данные';
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl('', [
         Validators.required,
