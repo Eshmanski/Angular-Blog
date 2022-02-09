feb6c36ab4422c49916dddcbded06f7398f46c72
Pavel Eshmanski <Eshmanski@gmail.com>
2022-02-09 17:59:01 +0300

    11: Add Error handling

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -14,7 +14,7 @@ export class LoginPageComponent implements OnInit {
   submitted: boolean = false;
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router
     ) { }
 
@@ -48,6 +48,8 @@ export class LoginPageComponent implements OnInit {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
       this.submitted = false;
-    })
+    }, () => {
+      this.submitted = false;
+    });
   }
 }
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {FbAuthResponse, User} from "../../../shared/interface";
-import {Observable, tap} from "rxjs";
+import {catchError, Observable, ObservableInput, Subject, tap, throwError} from "rxjs";
 import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class AuthService {
-  // token: string = '';
+  public error$: Subject<string> = new Subject<string>();
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(
+    private http: HttpClient,
+
+  ) {}
 
   get token(): string {
     const expDate = new Date(localStorage.getItem('fb-token-exp') || '');
@@ -27,7 +29,8 @@ export class AuthService {
 
     return this.http.post<FbAuthResponse>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
-        tap<FbAuthResponse>(this.setToken)
+        tap<FbAuthResponse>(this.setToken),
+        catchError(this.handleError.bind(this))
       );
   }
 
@@ -37,6 +40,27 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return !!this.token;
+  }
+
+  private handleError(error: HttpErrorResponse): ObservableInput<any>{
+    const { message } = error.error.error;
+
+    switch(message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный email');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный пароль');
+        break;
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Email не найден')
+        break;
+      default:
+        this.error$.next('Неизвестная ошибка')
+        break;
+    }
+
+    return throwError(() => error);
   }
 
   private setToken(res: FbAuthResponse | null): void {
