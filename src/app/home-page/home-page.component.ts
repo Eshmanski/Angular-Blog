507508238686c7662967340e0a674efdc9653572
Pavel Eshmanski <Eshmanski@gmail.com>
2022-02-09 22:39:19 +0300

    24: Implement output of posts list at Home Page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PostService} from "../shared/post.service";
+import {Observable} from "rxjs";
+import {Post} from "../shared/interface";
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  posts$: Observable<Post[]> = null!;
+
+  constructor(
+    private postsService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.posts$ = this.postsService.getAll()
   }
 
 }
