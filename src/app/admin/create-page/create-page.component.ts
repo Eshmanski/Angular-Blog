3705c2686bf7225a18e7190291f08b49f48aabb1
Pavel Eshmanski <Eshmanski@gmail.com>
2022-02-09 18:42:30 +0300

    13: Formalizetion Create post Page

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Post} from "../../shared/interface";
+
 
 @Component({
   selector: 'app-create-page',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePageComponent implements OnInit {
 
+  form: FormGroup = null!;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      text: new FormControl('', Validators.required),
+      author: new FormControl('', Validators.required),
+    })
+  }
+
+  submit() {
+    if(this.form .invalid) {
+      return
+    }
+
+    const post: Post = {
+      id: new Date().getTime().toString(),
+      title: this.form.value.title,
+      author: this.form.value.author,
+      text: this.form.value.text,
+      date: new Date(),
+    }
   }
 
 }
