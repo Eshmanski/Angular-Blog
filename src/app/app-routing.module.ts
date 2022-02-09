edc99a128a36f8254f96f3df0656081a8e0ab451
Pavel Eshmanski <Eshmanski@gmail.com>
2022-02-09 13:43:59 +0300

    04: Formalization main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     path: '', component: MainLayoutComponent, children: [
       { path: '', redirectTo: '/', pathMatch: 'full' },
       { path: '', component: HomePageComponent },
-      { path: 'path/:id', component: PostPageComponent }
+      { path: 'post/:id', component: PostPageComponent }
     ]
   },
   {
